c23e09318e9de6b62296101e88df8c298d10e51e
pikricypiral <pikriajadeh@gmail.com>
2022-08-06 10:32:15 +0700

    add fikri changes

--- src/components/customer1/BrandArea.jsx
+++ src/components/customer1/BrandArea.jsx
@@ -5,27 +5,27 @@ const BrandArea = () => {
   const brandImg = [
     {
       id: 1,
-      img: "assets/img/brand/brand-1.png",
+      img: "assets/img/brand/brand1.png",
     },
     {
       id: 2,
-      img: "assets/img/brand/brand-2.png",
+      img: "assets/img/brand/brand2.png",
     },
     {
       id: 3,
-      img: "assets/img/brand/brand-3.png",
+      img: "assets/img/brand/brand3.png",
     },
     {
       id: 4,
-      img: "assets/img/brand/brand-4.png",
+      img: "assets/img/brand/brand4.png",
     },
     {
       id: 5,
-      img: "assets/img/brand/brand-5.png",
+      img: "assets/img/brand/brand5.png",
     },
     {
       id: 6,
-      img: "assets/img/brand/brand-1.png",
+      img: "assets/img/brand/brand6.png",
     },
   ];
   return (
