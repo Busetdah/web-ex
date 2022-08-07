361061989c20c6c5283122e7659571875472c0f4
Busetdah <apriantowpj@gmail.com>
2022-08-07 22:25:32 +0700

    add product unicode

--- src/components/Contact/ContactAreaTwo.jsx
+++ src/components/Contact/ContactAreaTwo.jsx
@@ -85,7 +85,7 @@ const ContactAreaTwo = () => {
                       ></textarea>
                        <input type="file" accept="image/png, image/jpeg, image/jpg" name="menu_image" className="box"></input>
                       <div className="col-lg-6">
-                        <div className="contact-filed contact-icon-mail mb-20">
+                        <div className="contact-filed contact-icon-product mb-20">
                           
                           <input
                             email="text"
