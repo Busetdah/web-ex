374caaaec689d85c114f2ac24017c2b8c380f0eb
Busetdah <apriantowpj@gmail.com>
2022-08-25 08:53:53 +0700

    mboh

--- src/components/download/BlogArea.jsx
+++ src/components/download/BlogArea.jsx
@@ -37,9 +37,9 @@ On this page, you will find our brochures.
                   <div className="blog__item-2 blog__item-2-df mb-40">
                     <div className="blog__item-2-image">
                       <div className="blog__item-2-image-inner w-img">
-                        <Link href={`${blog.link}`} target="_blank">
+                        <a href={`${blog.link}`} target="_blank">
                           <a><img src={`${blog.imgTwo}`} alt="blog-img" /></a>
-                        </Link>
+                        </a>
                       </div>
                       
                     </div>
