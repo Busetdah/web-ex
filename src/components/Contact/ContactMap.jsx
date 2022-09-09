33e2086ebd2e947d3655efa0fe7e3e182650d02d
Mrakaa <rakamuhammadberani@gmail.com>
2022-09-09 14:56:01 +0700

    add raka changes

--- src/components/Contact/ContactMap.jsx
+++ src/components/Contact/ContactMap.jsx
@@ -5,7 +5,7 @@ const ContactMap = () => {
         <div className="contact__map-wrap">
           <iframe
             id="gmap_canvas"
-            src="https://maps.google.com/maps?q=Mission%20District%2C%20San%20Francisco%2C%20CA%2C%20USA&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed"
+            src="https://goo.gl/maps/Tvu49yt4g7rnBZsx9"
           ></iframe>
           <div className="contact__map-icon">
             <i className="fa-solid fa-location-dot"></i>
